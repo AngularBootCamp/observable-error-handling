b3a5624748d8efb2ed369b3506db8f5ac4aa9568
Lance Finney <lance.finney@oasisdigital.com>
2018-02-20 23:47:52 -0600

    curriculum update

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent }  from './app.component';
+import { EmployeesService }  from './employees.service';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [EmployeesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
