e5ff27adb4046f7dc470c40effdf46f678c254cf
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-08-08 11:53:25 -0500

    curriculum update

--- src/app/employees.service.ts
+++ src/app/employees.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { of, interval } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { interval, of } from 'rxjs';
 import { catchError, map, switchMap, tap } from 'rxjs/operators';
 
 const API_URL = 'https://api.angularbootcamp.com';
